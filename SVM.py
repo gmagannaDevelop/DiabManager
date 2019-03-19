497cebc585b49c12ac587850477de82b164ee2e2
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-03-19 11:14:23 +0100

    SVM is now using cross validation instead of the train/test/split approach. Mean score is around 0.66

--- SVM.py
+++ SVM.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
+import seaborn as sb
 
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 # the support vector machine class :  SVM  
 from sklearn.svm import SVC 
 
@@ -17,37 +20,104 @@ from sklearn.svm import SVC
 import gmparser as parse
 
 
-# In[50]:
+# In[2]:
 
 
 data, encoded_data, X, y = parse.main()
 
 
-# In[51]:
+# In[3]:
+
+
+data.head()
+
+
+# In[4]:
+
+
+#X
+
+
+# In[5]:
 
 
 X2 = preprocessing.scale(X)
 
 
-# In[80]:
+# In[6]:
+
+
+#X2
+
+
+# In[7]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(X2, y, random_state = 0) 
 
 
-# In[81]:
+# In[8]:
+
+
+kf = KFold(n_splits=5, shuffle=False).split(y)
+
+
+# In[72]:
+
+
+#print('Training', 7*'\t', 'Testing')
+#for i, j in kf:
+#    print(f'{i[0:4]}... \t{i[len(i)-5:len(i)]}, \t{j}')
+
+
+# In[ ]:
 
 
 svm_model_linear  = SVC(kernel = 'linear', C = 1).fit(X_train, y_train) 
 svm_model_rbf     = SVC(kernel = 'rbf', C = 1).fit(X_train, y_train) 
 svm_model_sigmoid = SVC(kernel = 'sigmoid', C = 1).fit(X_train, y_train) 
-svm_predictions   = svm_model_linear.predict(X_test) 
+svm_predictions   = svm_model_sigmoid.predict(X_test) 
 
 
-# In[82]:
+# In[9]:
 
 
-# model accuracy for X_test 
+kernels = ['linear', 'rbf', 'sigmoid']
+svc  = lambda x: SVC(kernel = x, C = 1, gamma = 'auto')
+SVMs = dict(
+    [(i, svc(i)) for i in kernels]
+)
+
+
+# In[10]:
+
+
+SVMs
+
+
+# In[14]:
+
+
+sigmoid_cvl = cross_val_score(SVMs['sigmoid'], X2, y, scoring='accuracy', cv = 15)
+
+
+# In[29]:
+
+
+print(f'Cross validation scores: \nMin: {sigmoid_cvl.min()}, Mean: {round(sigmoid_cvl.mean(),3)}, Max: {sigmoid_cvl.max()}')
+
+
+# In[16]:
+
+
+sb.distplot(sigmoid_cvl)
+
+
+# In[24]:
+
+
+## model accuracy for X_test (on a train_test_split scheme)
+'''
 linear_accuracy = svm_model_linear.score(X_test, y_test) 
 sbf_accuracy = svm_model_rbf.score(X_test, y_test) 
 sigmoid_accuracy = svm_model_sigmoid.score(X_test, y_test) 
@@ -55,52 +125,53 @@ sigmoid_accuracy = svm_model_sigmoid.score(X_test, y_test)
 
 # creating a confusion matrix 
 cm = confusion_matrix(y_test, svm_predictions) 
+'''
 
 
-# In[83]:
+# In[20]:
 
 
-linear_accuracy
+#linear_accuracy
 
 
-# In[84]:
+# In[21]:
 
 
-sbf_accuracy
+#sbf_accuracy
 
 
-# In[85]:
+# In[22]:
 
 
-sigmoid_accuracy
+#sigmoid_accuracy
 
 
-# In[66]:
+# In[19]:
 
 
-cm
+#cm
 
 
-# In[67]:
+# In[18]:
 
 
-data
+#data
 
 
-# In[33]:
+# In[17]:
 
 
 #scaled_data = preprocessing.scale(data)
-plot = pd.core.frame.DataFrame(index=data['postp tag'])
+#plot = pd.core.frame.DataFrame(index=data['postp tag'])
 #for i in data:
 #    plot[i] = data[i]
-plot['lol'] = list(data['activeInsulin'])
+#plot['lol'] = list(data['activeInsulin'])
 
 
-# In[35]:
+# In[ ]:
 
 
-plot.head()
+
 
 
 # In[ ]:
